8d8b2d619a80507c27a4b36846cd0657966fe6ea
talos <martin155mz@gmail.com>
2022-07-03 16:31:32 +0200

    link

--- src/main/advent-frontend/src/component/utils/ObjectToTable.js
+++ src/main/advent-frontend/src/component/utils/ObjectToTable.js
@@ -1,17 +1,16 @@
-export const ObjectToTable = ({ object }) => {
+import { Link } from 'react-router-dom'
+export const ObjectToTable = ({ object, routValue }) => {
+
     let objectAttributes = []
+    console.log(object)
     for (const prop in object) {
         if (object[prop] && prop !== "id") {
-            objectAttributes.push(<td key= {prop} name={prop}>{object[prop]}</td>)
+            objectAttributes.push(<td key={prop} name={prop}>{object[prop]}</td>)
         }
     }
     return (
-        <table>
-            <tbody>
-                <tr>
-                    {objectAttributes}
-                </tr>
-            </tbody>
-        </table>
+        <Link id={object.id} key={object.id} to={`/${routValue}/${object.id}`}>
+            <li className="listItem">{objectAttributes}</li>
+        </Link>
     )
 }
